13c7259603e21cc6863bbad569ae730c049500bc
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-12-27 14:24:54 +0100

    Updated test coverage script

--- scripts/coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-coffee -co lib src
-mkdir -p test-coverage
-rm -rf test-cov
-coffee -co spec-coverage spec/*.coffee
-jscoverage lib/ test-cov
-SRC_DIR=test-cov mocha --reporter html-cov spec-coverage/$1 > test-coverage/$2
-rm -rf spec-coverage test-cov
-open test-coverage/$2
