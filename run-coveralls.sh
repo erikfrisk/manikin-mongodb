d23163177b481efaca9798b55f9f0d9200b69d06
Jakob Mattsson <jakob.mattsson@gmail.com>
2013-08-25 00:07:11 +0200

    adding filepath to coveralls

--- run-coveralls.sh
+++ run-coveralls.sh
@@ -1,5 +1,5 @@
 if [ -z "$TRAVIS_JOB_ID" ]; then
   echo "Skipping coveralls"
 else
-  npm run coveralls | ./node_modules/coveralls/bin/coveralls.js
+  npm run coveralls | ./node_modules/coveralls/bin/coveralls.js src
 fi
